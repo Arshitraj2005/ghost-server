096109531623a6c0dbf18afc77dd2799145e6a26
Arshitraj2005 <nigachu789@gmail.com>
2025-07-26 02:00:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import subprocess
+import threading
+
 app = Flask(name)
+
+def run_stream():
+    subprocess.call(["python", "stream.py"])
+
+threading.Thread(target=run_stream).start()
 
 @app.route('/')
 def home():
-    return "Bot is alive"
+    return "Bot is alive and streaming!"
 
-if__name__== '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
